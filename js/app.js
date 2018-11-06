21ec57ac48d51286592314941270c8c8b14ed144
Tito <tito@atlasx.co>
2018-11-06 11:41:45 -0500

    cards styles

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ function loadTasks(taskList){
         divTask.innerHTML = `
             ${task.img?"<img class='task-card-image' src='" + task.img + "' />":""}
             <h4 class='task-card-status'> <input type="checkbox" ${task.completed?"checked":""} title="check completed">  ${task.title + ' ' + i}</h4>
-            <span class='task-card-created'>created on ${task.createdOn} by ${task.createdBy}</span>
+            <span class='task-card-created'>${task.createdOn ? 'created on' +  task.createdOn.toGMTString() : ''} ${task.createdBy ? 'by ' + task.createdBy: ''}</span>
             <p class='task-card-description'>${task.description}</p>
-            <span class='task-card-due'>Due on ${task.dueDate}</span>
+            <span class='task-card-due'>${task.dueDate ? 'Due on' + task.dueDate.toGMTString() : ''}</span>
         `;
 
         if(task.completed)
